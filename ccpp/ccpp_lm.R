0c8f677dc1cf7c078163839cfd777cd381067d3f
jeffedjekoomhene <j.edjekoomhene@gmail.com>
2025-06-05 08:03:28 +0200

    add boxplots

--- ccpp/ccpp_lm.R
+++ ccpp/ccpp_lm.R
@@ -133,10 +133,27 @@ Histo_Luftfeuchtigkeit <-
   theme(axis.text=element_text(size=8),
         axis.title=element_text(size=8))
 
+Histo_Stromproduktion <- 
+  ggplot(Daten, aes(x = PE )) +  
+  geom_histogram(fill = "orange", color = "black", binwidth=5) + 
+  labs(title = "unahängige Variable: \nStromproduktion", 
+       x = "Stromproduktion", 
+       y = "Häufigkeit")+  
+  scale_x_continuous(breaks = seq(0, max(Daten$PE), by = 25), labels = comma_format(big.mark = ".",decimal.mark = ","))+
+  geom_vline(aes(xintercept = mean(PE)), 
+             color = "darkred", 
+             linetype=1,
+             linewidth = 1)+
+  annotate("text", x = mean(Daten$PE), y = 8, label = "Mittelwert", 
+           angle = 90, vjust = 1.2, colour = "darkred", size = 3.5) +
+  theme(plot.title = element_text(size = 10, face = "bold")) + # Titel; Schriftgröße: 14, Fettdruck
+  theme(axis.text=element_text(size=8),
+        axis.title=element_text(size=8))
+
 #4 Create panel out of histograms
 library(multipanelfigure) # mehrere Bildchen in einem Raster anordnen
 
-Histogramme <- multi_panel_figure(rows = 2,
+Histogramme <- multi_panel_figure(rows = 3,
                                   columns = 2,
                                   panel_label_type = "none")
 
@@ -145,11 +162,67 @@ Histogramme %<>%
   fill_panel(Histo_Temperatur, row = 1, column = 1) %<>%
   fill_panel(Histo_Umgebungsdruck,           row = 1, column = 2) %<>%
   fill_panel(Histo_Luftfeuchtigkeit,        row = 2, column = 1) %<>%
-  fill_panel(Histo_Abluftvakuum,        row = 2, column = 2) 
-
+  fill_panel(Histo_Abluftvakuum,        row = 2, column = 2) %<>%
+  fill_panel(Histo_Stromproduktion,        row = 3, column = 1)
 print(Histogramme)
 
+#---------------------------------------------------------------
+#Boxplots
+#---------------------------------------------------------------
+boxplot_Stromproduktion <-
+  ggplot(Daten, aes(x = PE )) +  
+  geom_boxplot(fill="green")+
+  stat_boxplot(geom="errorbar")+
+  labs(title="Stromproduktion")+
+  theme(plot.title=element_text(hjust=0.5))+
+  coord_flip()
 
+boxplot_Temperatur <-
+  ggplot(Daten, aes(x = AT )) +  
+  geom_boxplot(fill="lightblue")+
+  stat_boxplot(geom="errorbar")+
+  labs(title="Temperatur")+
+  theme(plot.title=element_text(hjust=0.5))+
+  coord_flip()
+
+boxplot_Abluftvakuum <-
+  ggplot(Daten, aes(x = V )) +  
+  geom_boxplot(fill="lightblue")+
+  stat_boxplot(geom="errorbar")+
+  labs(title="Abluftvakuum")+
+  theme(plot.title=element_text(hjust=0.5))+
+  coord_flip()
+
+boxplot_Umgebungsdruck <-
+  ggplot(Daten, aes(x = AP )) +  
+  geom_boxplot(fill="lightblue")+
+  stat_boxplot(geom="errorbar")+
+  labs(title="Umgebungsdruck")+
+  theme(plot.title=element_text(hjust=0.5))+
+  coord_flip()
+
+boxplot_Luftfeuchtigkeit <-
+  ggplot(Daten, aes(x = RH )) +  
+  geom_boxplot(fill="lightblue")+
+  stat_boxplot(geom="errorbar")+
+  labs(title="Luftfeuchtigkeit")+
+  theme(plot.title=element_text(hjust=0.5))+
+  coord_flip()
+
+
+Boxplots <- multi_panel_figure(rows = 3,
+                               columns = 2,
+                               panel_label_type = "none")
+
+# wo soll welches Bild gezeigt werden?
+Boxplots %<>%
+  fill_panel(boxplot_Stromproduktion, row = 1, column = 1) %<>%
+  fill_panel(boxplot_Temperatur,  row = 1, column = 2) %<>%
+  fill_panel(boxplot_Abluftvakuum, row = 2, column = 1) %<>%
+  fill_panel(boxplot_Umgebungsdruck, row = 2, column = 2) %<>%
+  fill_panel(boxplot_Luftfeuchtigkeit, row = 3, column = 1) 
+
+Boxplots
 
 
 
